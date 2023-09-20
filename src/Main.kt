da314875015570967a112c21b6608c1ecaf96c2a
NexoBeats <nicomerzky@gmail.com>
2023-09-20 16:34:54 +0200

    message

--- src/Main.kt
+++ src/Main.kt
@@ -4,5 +4,6 @@ fun main() {
         println("$it ist effektive gegen "+ it.getStrengths())
         println("$it ist Schwach gegen "+it.getWeaknesses())
         println("$it ist nicht Wirksam gegen "+it.getIneffectiveness())
+        println()
     }
 }
