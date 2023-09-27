b4432270007576d181274c2dd91a89dff6c2aeb9
NexoBeats <nicomerzky@gmail.com>
2023-09-27 17:28:08 +0200

    es geht voran

--- src/Tackle.kt
+++ src/Tackle.kt
@@ -14,10 +14,10 @@ class Tackle(ap: Int, apd: Int, type: Types = Types.Normal, private var name: St
         val neuerGegner = pokemon.gegner
         if (neuerGegner != null) {
             if (pokemon.getHealth() > 0 && this.ap > 0) {
-                val isCritical: Boolean = (1..10).random() == 5
+                val isCritical: Boolean = (1..100).random() in listOf(25, 50, 75)
                 val pokemonTypes: List<Types> = neuerGegner.pokemonTypen
                 val effectiveness: Double = getEffectiveness(pokemonTypes.first())
-                val dmg = ((2 + ((0.2 * pokemon.atk) + (0.1 * apd))).roundToInt() * effectiveness).roundToInt() * if (isCritical) 4 else 1
+                val dmg = (((2 + (apd * 0.1) + (pokemon.atk * 0.5)).roundToInt() * effectiveness).roundToInt()) * if (isCritical) 4 else 1
                 if (effectiveness != 1.0) {
                     println("${pokemon.getName()} setzt " + this.name + " ein!")
                     println("Es ist ${if (effectiveness == 0.5){ "nicht sehr Effektiv" } else if (effectiveness == 2.0 ){ "sehr Effektiv" } else { "Wirkungslos" }} gegen ${neuerGegner.getName()}.")
